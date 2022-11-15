a18ffb87893c9f6db4adfbae49ebfef599713ed0
Darlish MBAMA <michelmbama01gmail.com>
2022-11-15 15:56:01 +0100

    n'importe quoi

---
+++ nom_tree.c
@@ -0,0 +1,5 @@
+//
+// Created by miche on 14/11/2022.
+//
+
+#include "nom_tree.h"
---
+++ nom_tree.h
@@ -0,0 +1,8 @@
+//
+// Created by miche on 14/11/2022.
+//
+
+#ifndef MAIN_C_NOM_TREE_H
+#define MAIN_C_NOM_TREE_H
+
+#endif //MAIN_C_NOM_TREE_H
